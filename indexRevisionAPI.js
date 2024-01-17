3da5d3cb72c576364c57d00843d135dcf56ea012
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-01-17 17:45:06 +0000

    db connection and query test

---
+++ indexRevisionAPI.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const {MongoClient, ObjectId} = require('mongodb')
+
+const app = express()
+const port = 3000
+app.use(express.json())
+
+async function connectToMongo() {
+    const connection = await MongoClient.connect("mongodb://root:password@localhost:27017")
+    return connection.db('revision')
+}
+
+// app.get('/', async (request, response) => {
+//         const db = await connectToMongo()
+//         const products = db.collection('questions_and_answers')
+//         const result = await products.insertOne({type: "answer", body: "RMBD have relationships between tables, have null fields and defined table structures, DODBs do not need any of these, it is highly flexible and useful for rapid development and prototyping.", qacode: 2})
+//         if (result.insertedID !== null) {
+//             response.send('it worked')
+//         } else {
+//             response.send('it broke')
+//         }
+//     }
+// )
+
+app.get('/qa', async (request, response) => {
+
+    const qaParams = request.query
+
+    function filterProducts(qaParams) {
+        const filter = {};
+
+        if (qaParams && qaParams.qacode) {
+            filter.qacode = parseInt(qaParams.qacode);
+        }
+        return filter;
+    }
+
+    try {
+        const db = await connectToMongo()
+        const qas = db.collection('questions_and_answers')
+        const result = await qas.find(filterProducts(qaParams)).toArray()
+        if (result.length > 0) {
+            response.json({
+                message: "Successfully question and answer",
+                data: result
+            });
+            response.status(200);
+        } else {
+            response.send({
+                message: "Unexpected error",
+                data: []
+            })
+        }
+    } catch {
+        response.status(500).send('Unexpected error');
+    }
+})
+
+
+app.listen(port);
