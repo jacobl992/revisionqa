bf2512443d3a89e7a9572fc7c9c0e6c1a258cff6
jacobl992 <jake_lewis12@hotmail.co.uk>
2024-01-18 17:14:47 +0000

    random question generated and shown

--- public/js/generateQAndA.js
+++ public/js/generateQAndA.js
@@ -1,20 +1,41 @@
+let qaCodes = [];
+let qaData = {};
 let randomQACode = 0;
 const qaBoxElement = document.querySelector('#qa-box');
 const questionElement = document.querySelector('#question-p');
 const dbAnswerElement = document.querySelector('#db-answer');
 
+const extractQacodes = (qacodeData) => {
+    return qacodeData.map(obj => obj.qacode);
+};
 function getRandomEntry(qaCodesArray) {
     const randomIndex = Math.floor(Math.random() * qaCodesArray.length);
     return qaCodesArray[randomIndex];
 }
 
-window.qaCodesPromise.then((qaCodes) => {
+async function retrieveQACodes (qaCodes) {
+    try {
+        const response = await fetch('/qacodes', {
+            method: 'GET',
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log('Response data:', data);
+        return extractQacodes(data.data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+document.querySelector('#generateQuestion').addEventListener('click', async (qaCodes) => {
+    //first part - getting list of poss Q's and A's
+    qaCodes = await retrieveQACodes();
     randomQACode = getRandomEntry(qaCodes);
-    console.log(randomQACode);
-}).catch((error) => {
-    console.error('Error:', error);
-});
-document.querySelector('#generateQuestion').addEventListener('click', async () => {
+
+    //second part - selecting a Q and A
     try {
         qaBoxElement.style.display = 'none';
         const url = `/qa?qacode=${randomQACode}`;
@@ -26,9 +47,9 @@ document.querySelector('#generateQuestion').addEventListener('click', async () =
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
-        const data = await response.json();
-        console.log('Response data:', data);
-        questionElement.innerHTML = data.data[0].body
+        qaData = await response.json();
+        console.log('Response data:', qaData);
+        questionElement.innerHTML = qaData.data[0].body
         qaBoxElement.style.display = 'block';
     } catch (error) {
         console.error('Error:', error);
